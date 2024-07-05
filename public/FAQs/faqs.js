aadbb3f22fdfe47b7ea95e2032aab5e98cf8d790
AbdulMoiz77 <148119216+AbdulMoiz77@users.noreply.github.com>
2024-07-06 00:17:15 +0500

    FAQs files

---
+++ public/FAQs/faqs.js
@@ -0,0 +1,10 @@
+let login = document.getElementsByClassName('login')[0]
+
+login.addEventListener('click', function() {
+    window.location.href = '../Login/login.html'
+})
+
+let signup = document.getElementsByClassName('signup')[0]
+signup.addEventListener('click', function(){
+    window.location.href = '../Signup/signup.html'
+})
