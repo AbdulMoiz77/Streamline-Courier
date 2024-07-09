02e5af64e467f6bcd93162fd3d7252730655d3b7
AbdulMoiz77 <148119216+AbdulMoiz77@users.noreply.github.com>
2024-07-09 15:32:05 +0500

    Shipping Interface files

---
+++ public/Shipping/ship.js
@@ -0,0 +1,16 @@
+let login = document.getElementsByClassName('login')[0]
+
+login.addEventListener('click', function() {
+    window.location.href = '../login/login.html'
+})
+
+let signup = document.getElementsByClassName('signup')[0]
+signup.addEventListener('click', function(){
+    window.location.href = '../signup/signup.html'
+})
+
+const shipButton = document.getElementById('ship')
+
+shipButton.addEventListener('click', function() {
+    window.location.href = '../login/login.html'
+})
